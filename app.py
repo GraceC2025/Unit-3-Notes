cedf1893471b9e0c315d064981179832c21451d7
GraceC2025 <grace.corathers2025@gbwl.org>
2025-01-21 20:10:23 +0000

    css styling

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # index route for web app
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/hello/')
 @app.route('/hello/<name_data>')
@@ -14,5 +14,5 @@ def hello_there(name_data=None):
     return render_template('hello_there.html', name=name_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5421)
     
