100cbabade20b48b313c50511d68f327942757bf
GraceC2025 <grace.corathers2025@gbwl.org>
2025-01-22 15:43:03 +0000

    added a lot

--- app.py
+++ app.py
@@ -8,11 +8,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+ # New functions
+@app.route('/about/')
+def about():
+     return render_template("about.html")
+
+@app.route('/contact/')
+def contact():
+     return render_template("contact.html")
+
 @app.route('/hello/')
 @app.route('/hello/<name_data>')
 def hello_there(name_data=None):
     return render_template('hello_there.html', name=name_data)
 
+@app.route('/api/data')
+def get_data():
+    return app.send_static_file('data.json')
+
+# allows u to click run button
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5421)
     
